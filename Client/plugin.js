54ab1e6d9f793caafda1eefdc06e807fd5898a20
Triston Stuart <triston.stuart@icloud.com>
2017-11-11 00:32:38 -0700

    Update

--- Client/plugin.js
+++ Client/plugin.js
@@ -2,6 +2,17 @@
 
 let serv = exports;
 
-serv.load = () => {
-    console.log('[Client Plugin] Loading Client Server...'.bold.green)
+serv.load = (settings, express) => {
+    let port = settings.port;
+    console.log(String('[Client Plugin] Loading Client Server on port : ' + port).bold);
+    let clientServer = express();
+
+    clientServer.use('/' + settings.serverStaticFilesPath, express.static('/' + settings.clientPath));
+    clientServer.get('/', (req, res) => {
+        res.sendFile(__dirname + '/' + settings.clientPath + '/' + settings.clientIndex + '.html')
+    });
+    clientServer.get('/test', (req, res) => {
+        res.send('Client is up and running!');
+    });
+    clientServer.listen(port)
 }
