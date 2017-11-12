0149e30fa3a5edfbc8b98978e4b8010312d5f18e
Triston Stuart <triston.stuart@icloud.com>
2017-11-12 13:16:50 -0700

    Update

--- Plugins/Client/Website/JS/blob.js
+++ Plugins/Client/Website/JS/blob.js
@@ -1,18 +1,36 @@
-function Blob(x, y, r) {
+function Blob(x, y, r, color) {
     this.pos = createVector(x, y);
     this.r = r;
-
-    this.update = function() {
-        let vel = createVector(mouseX-width/2, mouseY-height/2);
-        vel.setMag(3);
-        this.pos.add(vel);
+    if (color){
+        this.color = color;
+    }else {
+        this.color = [random(255), random(255), random(255)]
     }
 
-    this.eats = function(other){
+    this.update = function() {
+        if (mouseX <= (width/2 + 10) && mouseX >= (width/2 - 10) && mouseY <= (height/2 + 10) && mouseY >= (height/2 - 10)){
+            //nothing
+        }else {
+            let vel = createVector(mouseX-width/2, mouseY-height/2);
+            vel.setMag(3/(sqrt(this.r) / 32));
+            this.pos.add(vel);
+        }
+        this.r -= (0.0025 * (sqrt(this.r)/ 1.5))
+
+    }
+
+    this.eats = function(other, newCell){
         let d = p5.Vector.dist(this.pos, other.pos);
         if (d < this.r + other.r){
             let sum = PI * this.r * this.r + PI * other.r * other.r;
             this.r = sqrt(sum / PI)
+            if (newCell){
+                let x = random(-width, width);
+                let y = random(-height, height)
+                let R = random(50)
+                blobs[blobs.length] = new Blob(x, y, R);
+            }
+
             return true;
         }else {
             return false;
@@ -20,7 +38,14 @@ function Blob(x, y, r) {
     }
 
     this.show = function() {
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2)
+        fill(this.color[0], this.color[1], this.color[2], 127);
+        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+        textSize(this.r);
+        textAlign(CENTER);
+        text(this.mass(), this.pos.x, this.pos.y);
+    }
+
+    this.mass = function(){
+        return Math.floor(sqrt(PI * this.r * this.r))
     }
 }
--- Plugins/Client/Website/JS/sketch.js
+++ Plugins/Client/Website/JS/sketch.js
@@ -1,40 +1,95 @@
 let blob;
 let blobs = [];
 let scroll = 0.5;
+let zoom = 1;
+let ejectedMass = [];
+let EjectMe = 5;
 
 function setup() {
     createCanvas(window.innerWidth - 20, window.innerHeight - 35);
+    setFrameRate(240);
     blob = new Blob(0, 0, 64);
-    for (let i=0; i< 100; i++){
-        let x = random(-width, width);
-        let y = random(-height, height)
-        blobs[i] = new Blob(x, y, 16);
+    for (let i=0; i< 500; i++){
+        let x = random(-5000, 5000);
+        let y = random(-5000, 5000)
+        let R = random(32)
+        blobs[i] = new Blob(x, y, R);
     }
+    document.getElementById('fps').innerHTML = "FPS : " + Math.floor(frameRate());
+    setInterval(() => {
+        document.getElementById('fps').innerHTML = "FPS : " + Math.floor(frameRate());
+    }, 100)
 }
 
 function mouseWheel(event) {
-  scroll += event.delta / 500;
+    if ((scroll + event.delta/1000) > 0 && (scroll + event.delta/500) <= 7){
+        scroll += event.delta / 500;
+    }
 }
 
 function keyPressed() {
-  if (keyCode == ESCAPE) {
-    if (document.getElementById('overlay').style.visibility == "visible"){
-        document.getElementById('overlay').style.visibility = "hidden";
-    }else {
-        document.getElementById('overlay').style.visibility = "visible";
+    if (keyCode == ESCAPE) {
+        if (document.getElementById('overlay').style.visibility == "visible"){
+            document.getElementById('overlay').style.visibility = "hidden";
+        }else {
+            document.getElementById('overlay').style.visibility = "visible";
+        }
     }
-  }
 }
 
 function draw() {
     background(0);
+
+    if (keyIsDown(87)){
+        if (EjectMe == 5){
+            if (blob.r > 65){
+                let arrayId = ejectedMass.length;
+                ejectedMass[arrayId] = new Blob(blob.pos.x, blob.pos.y, 32, [blob.color[0], blob.color[1], blob.color[2]]);
+                ejectedMass[arrayId].killable = false;
+                ejectedMass[arrayId].projection = {
+                    cont: 20 + sqrt(blob.r),
+                    x : mouseX,
+                    y : mouseY,
+                    blobX : blob.pos.x,
+                    blobY : blob.pos.y
+                }
+                let sum = PI * blob.r * blob.r - PI * ejectedMass[arrayId].r * ejectedMass[arrayId].r;
+                blob.r = sqrt(sum / PI);
+            }
+            EjectMe = 0;
+        }else {
+            EjectMe++;
+        }
+
+    }
+
     translate(width/2, height/2);
-    scale(64 / (blob.r * scroll));
+    let newzoom = 64 / (blob.r * scroll);
+    zoom = lerp(zoom, newzoom, 0.08)
+    scale(zoom);
     translate(-blob.pos.x, -blob.pos.y)
     for (let i= blobs.length-1; i>=0;i--){
         blobs[i].show();
-        if (blob.eats(blobs[i])){
+        if (blob.eats(blobs[i], true)){
             blobs.splice(i, 1);
+        }
+    }
+    for (let ii= ejectedMass.length-1; ii>=0;ii--){
+        if (ejectedMass[ii].projection.killable && blob.eats(ejectedMass[ii], false)){
+            ejectedMass.splice(ii, 1);
+        }else {
+            if (ejectedMass[ii].projection.cont > 0){
+                for (let iii=0; iii < 2; iii++){
+                    let vel = createVector(ejectedMass[ii].projection.x-width/2, ejectedMass[ii].projection.y-height/2);
+                    vel.setMag(3 + sqrt(blob.r));
+                    ejectedMass[ii].pos.add(vel);
+                    ejectedMass[ii].projection.cont--;
+                }
+
+            }else {
+                ejectedMass[ii].projection.killable = true;
+            }
+            ejectedMass[ii].show();
         }
     }
     blob.show();
