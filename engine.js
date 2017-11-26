ee562b1b5d1c421bbe21e7bf8f9f9bb3b9fdddfd
Triston Stuart <triston.stuart@icloud.com>
2017-11-25 23:38:37 -0700

    Layout + Mouse Update

---
+++ engine.js
@@ -0,0 +1,12 @@
+let vector = require('vector').Vector;
+
+let cell = {
+    update : {
+        mouse : (blob, mouseX, mouseY, width, height) => {
+            let vel = new vector(mouseX-(width/2), mouseY-(height/2));
+            vel.setMag(3/(sqrt(blob.radius) / 32));
+            blob.x += vel.x;
+            blob.y += vel.y;
+        }
+    }
+}
