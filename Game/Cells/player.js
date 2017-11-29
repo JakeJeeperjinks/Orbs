e15f6ba641b1ff9ce0293dd81bf1f6b954b6cc09
Triston Stuart <triston.stuart@icloud.com>
2017-11-28 19:22:36 -0700

    small update

--- Game/Cells/player.js
+++ Game/Cells/player.js
@@ -1,12 +1,16 @@
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-exports.create = function(name, mass, gameX, gameY, color){
+exports = function(name, mass, gameX, gameY, color){
+    // Whole Player setup
     this.name = name;
     this.color = color;
     this.cells = [];
+
+    // Setup 'Main' Cell
     this.cells[0] = {};
     this.cells[0].x = random(-gameX, gameX);
     this.cells[0].y = random(-gameY, gameY);
     this.cells[0].mass = mass;
+    this.cells[0].radius = 
 }
